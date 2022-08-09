1d22cf5073b36637d61e0bb483a784a67cef44b4
Brian Hong <ebrianhong@gmail.com>
2022-08-08 20:55:59 -0700

    react api practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    fetchPeople(5);
+  }, []);
+
+  const fetchPeople = async (num) => {
+    const url = `https://randomuser.me/api/?results=${num}`;
+    // try {
+    //   const res = await fetch(url);
+    //   const json = await res.json();
+    //   setPersons(json.results);
+    // } catch(err) {
+    //   console.log('err', err);
+    // }
+    fetch(url)
+      .then(res => res.json())
+      .then(json => setPersons(json.results))
+      .catch(err => console.log('err', err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {persons ? 
+        persons.map(person => {
+          return <div key={person.id.value}>{person.name.title} {person.name.first} {person.name.last}</div>
+        })
+        : 'loading'
+      }
     </div>
   );
 }
